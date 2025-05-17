4ebec1c770d7546366141b4189465ec13f62e87d
fanzila-cyber <zilayy.sch>
2025-05-17 17:41:54 +0800

    tugas

---
+++ PHP/tugas12.php
@@ -0,0 +1,25 @@
+<?php
+
+$i = 1;
+
+$AnggotaKelas10 = [
+    "Akifah",
+    "Hera",
+    "Zahrah",
+    "Firqah",
+    "Zila",
+    "Fahra",
+    "Zizi",
+    "Jila",
+    "Nadia",
+    "Adawiyah",
+    "Alifah",
+    "Muftihah",
+    "Auliyah",
+    "Putri"
+];
+
+echo "Nama-Nama Anggota Kelas 10  </br>";
+foreach ($AnggotaKelas10 as $Nama_Nama_Kelas10) {
+    echo $i++ ." ." .$Nama_Nama_Kelas10 ."</br>";
+}
